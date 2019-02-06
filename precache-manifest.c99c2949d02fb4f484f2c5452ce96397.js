608b8166ce04957d6cac89dac6da68a3c28f5915
unknown <D:\PortableProgs\TheBatPortable\Data\Mail>
2019-02-06 15:31:54 +0300

    first commit

---
+++ precache-manifest.c99c2949d02fb4f484f2c5452ce96397.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a9a261c8b8dfe90db11f1817a9d22e1",
+    "url": "/alarm/fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.8a9a261c.woff2"
+  },
+  {
+    "revision": "83472592e1b6bbf74ed01bc8bba6af8a",
+    "url": "/alarm/index.html"
+  },
+  {
+    "revision": "a9ff913a3c17b881b663",
+    "url": "/alarm/js/4b47640d.21cfc388.js"
+  },
+  {
+    "revision": "a13b2bd5261e2df374128df1aab93001",
+    "url": "/alarm/statics/icons/icon-512x512.png"
+  },
+  {
+    "revision": "77fc0a1ba9903d27a272",
+    "url": "/alarm/js/6959fb28.f9b81025.js"
+  },
+  {
+    "revision": "cb4aad42e7ce9cd3741a69300cd7ad03",
+    "url": "/alarm/statics/sounds/sound.mp3"
+  },
+  {
+    "revision": "557df40f71ecdaa17cc8",
+    "url": "/alarm/js/app.694f9fb7.js"
+  },
+  {
+    "revision": "2debaeca3dc0ecfe7c52",
+    "url": "/alarm/js/vendor.00146eb4.js"
+  },
+  {
+    "revision": "0c2aa05997b549ae22d32e096912de9e",
+    "url": "/alarm/statics/sounds/sound2.ogg"
+  },
+  {
+    "revision": "66d7719a8ca03c1a591444c013802817",
+    "url": "/alarm/statics/icons/icon-384x384.png"
+  },
+  {
+    "revision": "ed12d10fd0407eea42ccaf7af7991146",
+    "url": "/alarm/statics/icons/ms-icon-144x144.png"
+  },
+  {
+    "revision": "20c6420ccf16a07d9d56538e30a6b524",
+    "url": "/alarm/statics/icons/icon-256x256.png"
+  },
+  {
+    "revision": "8c4120b4e88276badbc808e518572fad",
+    "url": "/alarm/statics/icons/icon-192x192.png"
+  },
+  {
+    "revision": "da2b422e2895b9b9c1782a71b6be9d63",
+    "url": "/alarm/statics/icons/apple-icon-152x152.png"
+  },
+  {
+    "revision": "4e56b1fa6ea3221ec65b",
+    "url": "/alarm/js/02e5d87b.1a6ebbab.js"
+  },
+  {
+    "revision": "3020c8ac2c2872dec7741e5948520093",
+    "url": "/alarm/statics/icons/icon-128x128.png"
+  },
+  {
+    "revision": "efb00e57304380d857603760600da5d5",
+    "url": "/alarm/statics/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "2f9d7587924933c1912c342b190b53ca",
+    "url": "/alarm/statics/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "3020c8ac2c2872dec7741e5948520093",
+    "url": "/alarm/statics/quasar-logo.png"
+  },
+  {
+    "revision": "c38ebd3cd38c98fbd16bf31d1d24ce64",
+    "url": "/alarm/fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNa.c38ebd3c.woff"
+  },
+  {
+    "revision": "bafb105baeb22d965c70fe52ba6b49d9",
+    "url": "/alarm/fonts/KFOmCnqEu92Fr1Mu4mxM.bafb105b.woff"
+  },
+  {
+    "revision": "8c2ade503b34e31430d6c98aa29a52a3",
+    "url": "/alarm/fonts/KFOlCnqEu92Fr1MmYUtfBBc-.8c2ade50.woff"
+  },
+  {
+    "revision": "cf6613d1adf490972c557a8e318e0868",
+    "url": "/alarm/fonts/KFOlCnqEu92Fr1MmWUlfBBc-.cf6613d1.woff"
+  },
+  {
+    "revision": "a1471d1d6431c893582a5f6a250db3f9",
+    "url": "/alarm/fonts/KFOlCnqEu92Fr1MmSU5fBBc-.a1471d1d.woff"
+  },
+  {
+    "revision": "de8b7431b74642e830af4d4f4b513ec9",
+    "url": "/alarm/fonts/KFOlCnqEu92Fr1MmEU9fBBc-.de8b7431.woff"
+  },
+  {
+    "revision": "e9dbbe8a693dd275c16d32feb101f1c1",
+    "url": "/alarm/fonts/KFOkCnqEu92Fr1MmgVxIIzQ.e9dbbe8a.woff"
+  },
+  {
+    "revision": "557df40f71ecdaa17cc8",
+    "url": "/alarm/css/app.6a1779fe.css"
+  },
+  {
+    "revision": "77fc0a1ba9903d27a272",
+    "url": "/alarm/css/6959fb28.9ce42c0f.css"
+  },
+  {
+    "revision": "4e56b1fa6ea3221ec65b",
+    "url": "/alarm/css/02e5d87b.e01c905d.css"
+  }
+];
